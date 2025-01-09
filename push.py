352e817ba43326a2bcab9ef6d610d5315c0ecca5
ZGarry <1160564525@qq.com>
2025-01-09 10:48:37 +0800

    Automatic commit

--- push.py
+++ push.py
@@ -4,6 +4,7 @@ def get_date_str():
     date_string = f"{now.year}-{now.month:02d}-{now.day:02d}"
     return date_string
 
+
 def quickPush():
     # 移动到当前目录
     import os
@@ -20,7 +21,7 @@ def quickPush():
 
     # 推送到远程仓库的main分支(github默认main，其他工具可能不同)
     origin = repo.remote(name='origin')
-    origin.push('master')
+    origin.push('main')
 
 
 if __name__ == "__main__":
